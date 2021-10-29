8533122b106ef09f6096095586c5a2e2e5ffcdc5
Yogesh Patel <yogeshpatel@Yogeshs-MacBook-Pro.local>
2021-10-28 21:38:59 -0400

    (update) login logic -- Need firebase to make functional

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App/App';
-import reportWebVitals from './reportWebVitals';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +12,3 @@ ReactDOM.render(
   document.getElementById('root'),
   () => document.title = "📜 Oh Sheet!"
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
