f4a1815193dde538543bbf5e9cf9db1ef1d67d56
yogesh98 <yogesh.patel0221@gmail.com>
2022-06-15 21:30:06 -0400

    testing

--- src/components/Cue/CueComponent.tsx
+++ src/components/Cue/CueComponent.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import {
     ICue,
 } from "types/types";
-import { Box, Button, Flex, Heading, Text,  useColorModeValue} from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Text,  useColorModeValue, useBreakpointValue,} from '@chakra-ui/react';
 import { WidthProvider, Responsive } from "react-grid-layout";
 import 'react-grid-layout/css/styles.css' 
 import 'react-resizable/css/styles.css' 
@@ -25,29 +26,47 @@ function CueDraggablePiece(props: {title: string, description: string}) {
             boxShadow={{ base: 'none', sm: useColorModeValue('lg', 'dark-lg') }}
             direction={'column'}
         >
-            <Heading size="md">{props.description}</Heading>
-            <Text>{props.title}</Text>
+            <Text fontSize={{ base: '14px', md: '20px', lg: '112x' }}>{props.description}</Text>
+            <Text fontSize={{ base: '10px', md: '15px', lg: '100x' }}>{props.title}</Text>
         </Flex>
     );
 }
 
 export default function CueComponent (props: ICueComponentProps) {
+    const [currentLayouts , setCurrentLayouts] = useState(props.layouts);
 
     const resetLayout = () => {
         props.onLayoutChange({});
     }
 
+    const onLayoutChange = (layouts: any) => {
+        props.onLayoutChange(layouts);
+        resetToLastLayoutThatRespectedHeight();
+    }
+
+    const resetToLastLayoutThatRespectedHeight = () => {
+        const grid = document.querySelector(".react-grid-layout") as HTMLElement | null;
+        const boundingBox = document.getElementById('responsive-grid-bounding-box') as HTMLElement | null;
+        const newHeight = grid?.offsetHeight;
+        const boundingBoxHeight = boundingBox?.offsetHeight;
+        console.log(newHeight, grid, boundingBoxHeight);
+        // if (newHeight > this.maxAllowedGridHeight) {
+        //     onLayoutChange(lastLayout);
+        // }
+    }
+
     return (
-        <Box h="100%" w="100%">
+        <Box id="responsive-grid-bounding-box" h="100%" w="100%">
             <Button onClick={resetLayout}>Reset Layout</Button>
             <ResponsiveReactGridLayout
-                className="layout"
-                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 2 }}
-                rowHeight={50}
+                cols={{ lg: 12, md: 10, sm: 6, xs: 4, xxs: 3 }}
+                rowHeight={60}
                 layouts={props.layouts}
                 onLayoutChange={(layout, layouts) =>
-                    props.onLayoutChange(layouts)
+                    onLayoutChange(layouts)
                 }
+                onResizeStart={setCurrentLayouts}
+                onDragStart={setCurrentLayouts}
             >
                 {Object.keys(props.cue).map((cueTitle, index) => {
                     return (
