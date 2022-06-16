1260e58bae3ccf486c4b68e4de77e345a73c5aab
yogesh98 <yogesh.patel0221@gmail.com>
2022-06-15 21:41:24 -0400

    (Update) resizing

--- src/components/Cue/CueCarouselComponent.tsx
+++ src/components/Cue/CueCarouselComponent.tsx
@@ -1,5 +1,4 @@
-// import {useState} from 'react';
-import { Box, Grid, GridItem, Heading } from "@chakra-ui/react";
+import { Box, Grid, GridItem, Heading, useBreakpointValue } from "@chakra-ui/react";
 import CueComponent from "components/Cue/CueComponent";
 import {
     ICue,
@@ -13,20 +12,20 @@ export interface ICueCarouselComponentProps {
 }
 
 export function CueCarouselComponent (props: ICueCarouselComponentProps) {
-    // return null;
     const currentCue = props.cues[props.currentPtr];
     const prevCue = props.currentPtr > 0 ?  props.cues[props.currentPtr - 1] : null;
     const nextCue = props.currentPtr < props.cues.length ? props.cues[props.currentPtr + 1] : null;
+    const renderPrevious = useBreakpointValue({ lg: true, md: false, sm: false, xs: false, xxs: false });
     return (
-        <Box id={"cue_carousel"} flex={props.flex} overflow={"auto"} h={"100%"} className={props.className}>
+        <Box id={"cue_carousel"} flex={props.flex} h={"100%"} className={props.className}>
             <Grid
             h='100%'
-            templateColumns='repeat(9, 1fr)'
+            templateColumns={useBreakpointValue({ lg: 'repeat(6, 1fr)', md: 'repeat(4, 1fr)', sm: 'repeat(4, 1fr)', xs: 'repeat(4, 1fr)', xxs: 'repeat(4, 1fr)' })}
             templateRows='repeat(15, 1fr)'
             gap={4}
             >
-                <GridItem  
-                    colSpan={3} 
+                {renderPrevious ? <GridItem  
+                    colSpan={2} 
                     borderWidth='2px'
                     borderRadius='lg'
                     borderColor="red.500" 
@@ -35,9 +34,9 @@ export function CueCarouselComponent (props: ICueCarouselComponentProps) {
                     alignItems={'center'}
                 >
                     <Heading size={"md"}>Previous</Heading>
-                </GridItem>
+                </GridItem> : null}
                 <GridItem  
-                    colSpan={3} 
+                    colSpan={2} 
                     borderWidth='2px'
                     borderRadius='lg'
                     borderColor="green.500" 
@@ -48,7 +47,7 @@ export function CueCarouselComponent (props: ICueCarouselComponentProps) {
                     <Heading size={"md"}>Current</Heading>
                 </GridItem>
                 <GridItem  
-                    colSpan={3} 
+                    colSpan={2} 
                     borderWidth='2px'
                     borderRadius='lg'
                     borderColor="blue.500" 
@@ -58,8 +57,8 @@ export function CueCarouselComponent (props: ICueCarouselComponentProps) {
                 >
                     <Heading size={"md"}>Next</Heading>
                 </GridItem>
-                <GridItem  
-                    colSpan={3} 
+                {renderPrevious ? <GridItem  
+                    colSpan={2} 
                     rowSpan={14}
                     borderWidth='2px'
                     borderRadius='lg'
@@ -69,9 +68,9 @@ export function CueCarouselComponent (props: ICueCarouselComponentProps) {
                     alignItems={'center'}
                 >
                     {prevCue ?  <CueComponent cue={prevCue} /> : <Heading size={"md"}>N/A</Heading>}
-                </GridItem>
+                </GridItem> : null}
                 <GridItem  
-                    colSpan={3} 
+                    colSpan={2} 
                     rowSpan={14}
                     borderWidth='2px'
                     borderRadius='lg'
@@ -83,7 +82,7 @@ export function CueCarouselComponent (props: ICueCarouselComponentProps) {
                     {currentCue ?  <CueComponent cue={currentCue} /> : <Heading size={"md"}>N/A</Heading>}
                 </GridItem>
                 <GridItem  
-                    colSpan={3} 
+                    colSpan={2} 
                     rowSpan={14}
                     borderWidth='2px'
                     borderRadius='lg'
